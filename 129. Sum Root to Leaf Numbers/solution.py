f746efa204088d34dcbb8aca97bb7019c955d201
Simo <linsimo.mark@gmail.com>
2020-12-25 15:30:36 -0500

    129

---
+++ 129. Sum Root to Leaf Numbers/solution.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        print("hello world")
+        return 1
