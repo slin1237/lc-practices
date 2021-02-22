22e5bb0b91cfdc32a925befe18e0c98396aedcbe
Simo <linsimo.mark@gmail.com>
2021-02-22 07:32:02 -0800

    2 sum again

---
+++ 1. Two Sum/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        :param nums: List of integers
+        :param target: integer
+        :return: list of index
+        """
+        if not nums or len(nums) < 2:
+            return []
+        res_map = {}
+        for i in range(len(nums)):
+            remainder = target - nums[i]
+            if remainder in res_map:
+                return [i, remainder[remainder]]
+            else:
+                res_map[nums[i]] = i
+        return []
